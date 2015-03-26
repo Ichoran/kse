a6eed795a66eb6b3cf17e6601946ab28838a8ecb
Rex Kerr <ichoran@gmail.com>
2015-03-26 15:34:42 -0700

    Changed from `_tok` and `tok_` methods to just `not` method which returns the index of the next delimiter or of the end, whichever comes first.
    (`tok_` and `_tok` are just composed of `apply` and `not`; the gain in efficiency by knowing that the other was just run is minimal, especially given the use cases in Grok.)

--- eio/Delimiter.scala
+++ eio/Delimiter.scala
@@ -7,17 +7,15 @@ import kse.coll.packed._
 
 trait Delimiter {
   def apply(s: String, i0: Int, iN: Int, n: Int): Int
-  def _tok(s: String, i0: Int, iN: Int, n: Int): Long
-  def tok_(s: String, i0: Int, iN: Int, n: Int): Long
+  def not(s: String, i0: Int, iN: Int): Int
   def apply(ab: Array[Byte], i0: Int, iN: Int, n: Int): Int
-  def _tok(ab: Array[Byte], i0: Int, iN: Int, n: Int): Long
-  def tok_(ab: Array[Byte], i0: Int, iN: Int, n: Int): Long
+  def not(ab: Array[Byte], i0: Int, iN: Int): Int
   
   def terminatedBy(d2: Delimiter) = new TerminatedDelim(this, d2)
 }
 
 final class CharDelim(private val c: Char) extends Delimiter {
-  final def apply(s: String, i0: Int, iN: Int, n: Int): Int = {
+  final def apply(s: String, i0: Int, iN: Int, n: Int): Int =
     if (i0 >= iN) -1-iN
     else {
       var i = i0
@@ -25,8 +23,7 @@ final class CharDelim(private val c: Char) extends Delimiter {
       while (i < iN && k > 0 && s.charAt(i) == c) { i += 1; k -= 1 }
       i
     }
-  }
-  final def apply(ab: Array[Byte], i0: Int, iN: Int, n: Int): Int = {
+  final def apply(ab: Array[Byte], i0: Int, iN: Int, n: Int): Int =
     if (i0 >= iN) -1-iN
     else {
       var i = i0
@@ -34,57 +31,26 @@ final class CharDelim(private val c: Char) extends Delimiter {
       while (i < iN && k > 0 && ab(i) == c) { i += 1; k -= 1 }
       i
     }
-  }
-  final def _tok(s: String, i0: Int, iN: Int, n: Int): Long = {
-    var i = i0
-    var k = n
-    while (i < iN && k > 0 && s.charAt(i) == c) { i += 1; k -= 1 }
-    if (i >= iN) {
-      return if (i == i0) (-1-iN packII -1-iN).L else (i packII i).L
+  final def not(s: String, i0: Int, iN: Int): Int =
+    if (i0 >= iN) -1-iN
+    else {
+      var i = i0
+      while (i < iN && s.charAt(i) != c) i += 1
+      i
     }
-    var j = i+1
-    while (j < iN && s.charAt(j) != c) j += 1
-    (i packII j).L
-  }
-  final def _tok(ab: Array[Byte], i0: Int, iN: Int, n: Int): Long = {
-    var i = i0
-    var k = n
-    while (i < iN && k > 0 && ab(i) == c) { i += 1; k -= 1 }
-    if (i >= iN) {
-      return if (i == i0) (-1-iN packII -1-iN).L else (i packII i).L
+  final def not(ab: Array[Byte], i0: Int, iN: Int): Int =
+    if (i0 >= iN) -1-iN
+    else {
+      var i = i0
+      while (i < iN && ab(i) != c) i += 1
+      i
     }
-    var j = i+1
-    while (j < iN && ab(j) != c) j += 1
-    (i packII j).L
-  }
-  final def tok_(s: String, i0: Int, iN: Int, n: Int): Long = {
-    var i = i0
-    while (i < iN && s.charAt(i) != c) i += 1
-    if (i >= iN) {
-      return if (i == i0) (-1-iN packII -1-iN).L else (i packII i).L
-    }
-    var j = i+1
-    var k = n-1
-    while (j < iN && k > 0 && s.charAt(j) == c) j += 1
-    (i packII j).L
-  }
-  final def tok_(ab: Array[Byte], i0: Int, iN: Int, n: Int): Long = {
-    var i = i0
-    while (i < iN && ab(i) != c) i += 1
-    if (i >= iN) {
-      return if (i == i0) (-1-iN packII -1-iN).L else (i packII i).L
-    }
-    var j = i+1
-    var k = n-1
-    while (j < iN && k > 0 && ab(j) == c) j += 1
-    (i packII j).L
-  }
 }
 
 final class WhiteDelim extends Delimiter  {
   @inline final def whiteByte(b: Byte) = ((b & 0xFF) <= 32) && ((1L << b) & 0x1f0003e00L) != 0
   @inline final def darkByte(b: Byte) = ((b & 0xFF) > 32) || ((1L << b) & 0x1f0003e00L) == 0
-  final def apply(s: String, i0: Int, iN: Int, n: Int): Int = {
+  final def apply(s: String, i0: Int, iN: Int, n: Int): Int =
     if (i0 >= iN) -1-iN
     else {
       var i = i0
@@ -92,64 +58,32 @@ final class WhiteDelim extends Delimiter  {
       while (i < iN && k > 0 && Character.isWhitespace(s.charAt(i))) { i += 1; k -= 1 }
       i
     }
-  }
-  final def apply(ab: Array[Byte], i0: Int, iN: Int, n: Int): Int = {
+  final def apply(ab: Array[Byte], i0: Int, iN: Int, n: Int): Int =
    if (i0 >= iN) -1-iN
    else {
-     var i = i0
-     var k = n
-     while (i < iN && k > 0 && whiteByte(ab(i))) { i += 1; k -= 1 }
-     i
-   }
-  }
-  final def _tok(s: String, i0: Int, iN: Int, n: Int): Long = {
-    var i = i0
-    var k = n
-    while (i < iN && k > 0 && Character.isWhitespace(s.charAt(i))) { i += 1; k -= 1 }
-    if (i >= iN) {
-      return if (i == i0) (-1-iN packII -1-iN).L else (i packII i).L
+      var i = i0
+      var k = n
+      while (i < iN && k > 0 && whiteByte(ab(i))) { i += 1; k -= 1 }
+      i
     }
-    var j = i+1
-    while (j < iN && !Character.isWhitespace(s.charAt(j))) j += 1
-    (i packII j).L
-  }
-  final def _tok(ab: Array[Byte], i0: Int, iN: Int, n: Int): Long = {
-    var i = i0
-    var k = n
-    while (i < iN && k > 0 && whiteByte(ab(i))) { i += 1; k -= 1 }
-    if (i >= iN) {
-      return if (i == i0) (-1-iN packII -1-iN).L else (i packII i).L
+  final def not(s: String, i0: Int, iN: Int): Int =
+    if (i0 >= iN) -1-iN
+    else {
+      var i = i0
+      while (i < iN && !Character.isWhitespace(s.charAt(i))) i += 1
+      i
     }
-    var j = i+1
-    while (j < iN && darkByte(ab(j))) j += 1
-    (i packII j).L
-  }
-  final def tok_(s: String, i0: Int, iN: Int, n: Int): Long = {
-    var i = i0
-    while (i < iN && !Character.isWhitespace(s.charAt(i))) i += 1
-    if (i >= iN) {
-      return if (i == i0) (-1-iN packII -1-iN).L else (i packII i).L
+  final def not(ab: Array[Byte], i0: Int, iN: Int): Int =
+    if (i0 >= iN) -1-iN
+    else {
+      var i = i0
+      while (i < iN && darkByte(ab(i))) i += 1
+      i
     }
-    var j = i+1
-    var k = n-1
-    while (j < iN && k > 0 && Character.isWhitespace(s.charAt(j))) j += 1
-    (i packII j).L
-  }
-  final def tok_(ab: Array[Byte], i0: Int, iN: Int, n: Int): Long = {
-    var i = i0
-    while (i < iN && darkByte(ab(i))) i += 1
-    if (i >= iN) {
-      return if (i == i0) (-1-iN packII -1-iN).L else (i packII i).L
-    }
-    var j = i+1
-    var k = n-1
-    while (j < iN && k > 0 && whiteByte(ab(j))) j += 1
-    (i packII j).L
-  }
 }
 
 final class LineDelim extends Delimiter  {
-  final def apply(s: String, i0: Int, iN: Int, n: Int): Int = {
+  final def apply(s: String, i0: Int, iN: Int, n: Int): Int =
     if (i0 >= iN) -1-iN
     else {
       var i = i0
@@ -165,8 +99,7 @@ final class LineDelim extends Delimiter  {
       }
       i
     }
-  }
-  final def apply(ab: Array[Byte], i0: Int, iN: Int, n: Int): Int = {
+  final def apply(ab: Array[Byte], i0: Int, iN: Int, n: Int): Int =
     if (i0 >= iN) -1-iN
     else {
       var i = i0
@@ -182,43 +115,20 @@ final class LineDelim extends Delimiter  {
       }
       i
     }
-  }
-  final def _tok(s: String, i0: Int, iN: Int, n: Int): Long = {
-    val i1 = apply(s, i0, iN, n)
-    if (i1 >= iN) {
-      return if (i1 == i0) (-1-iN packII -1-iN).L else (i1 packII i1).L
+  final def not(s: String, i0: Int, iN: Int): Int = 
+    if (i0 >= iN) -1-iN
+    else {
+      var i = i0
+      while (i < iN && { val c = s.charAt(i); c != '\n' && c != '\r' }) i += 1
+      i
     }
-    var j = i1+1
-    while (j < iN && {val c = s.charAt(j); c != '\n' && c != '\r'}) j += 1
-    (i1 packII j).L
-  }
-  final def _tok(ab: Array[Byte], i0: Int, iN: Int, n: Int): Long = {
-    val i1 = apply(ab, i0, iN, n)
-    if (i1 >= iN) {
-      return if (i1 == i0) (-1-iN packII -1-iN).L else (i1 packII i1).L
+  final def not(ab: Array[Byte], i0: Int, iN: Int): Int =
+    if (i0 >= iN) -1-iN
+    else {
+      var i = i0
+      while (i < iN && { val c = ab(i); c != '\n' && c != '\r'}) i += 1
+      i
     }
-    var j = i1+1
-    while (j < iN && {val c = ab(j); c != '\n' && c != '\r'}) j += 1
-    (i1 packII j).L
-  }
-  final def tok_(s: String, i0: Int, iN: Int, n: Int): Long = {
-    var i = i0
-    while (i < iN &&  {val c = s.charAt(i); c != '\n' && c != '\r'}) i += 1
-    if (i >= iN) {
-      return if (i == i0) (-1-iN packII -1-iN).L else (i packII i).L
-    }
-    val i1 = apply(s, i, iN, n)
-    (i packII i1).L
-  }
-  final def tok_(ab: Array[Byte], i0: Int, iN: Int, n: Int): Long = {
-    var i = i0
-    while (i < iN &&  {val c = ab(i); c != '\n' && c != '\r'}) i += 1
-    if (i >= iN) {
-      return if (i == i0) (-1-iN packII -1-iN).L else (i packII i).L
-    }
-    val i1 = apply(ab, i, iN, n)
-    (i packII i1).L
-  }
 }
 
 final class TerminatedDelim(tokenizer: Delimiter, terminator: Delimiter) extends Delimiter {
@@ -230,16 +140,14 @@ final class TerminatedDelim(tokenizer: Delimiter, terminator: Delimiter) extends
     if (!(s eq cachedData) || (cachedStart > i0) || (cachedEnd < i0)) {
       cachedData = s
       cachedStart = i0
-      val term = terminator.tok_(s, i0, iN, 1).inLong
-      cachedEnd = term.i0 match { case -1 => i0; case x if x == term.i1 => iN+1; case x => x }
+      cachedEnd = terminator.not(s, i0, iN) match { case x => if (x < 0) i0 else x }
     }
   }
   private def loadCaches(ab: Array[Byte], i0: Int, iN: Int) {
     if (!(ab eq cachedData) || (cachedStart > i0)) {
       cachedData = ab
       cachedStart = i0
-      val term = terminator.tok_(ab, i0, iN, 1).inLong
-      cachedEnd = term.i0 match { case -1 => i0; case x if x == term.i1 => iN+1; case x => x }
+      cachedEnd = terminator.not(ab, i0, iN) match { case x => if (x < 0) i0 else x }
     }
   }
     
@@ -253,31 +161,15 @@ final class TerminatedDelim(tokenizer: Delimiter, terminator: Delimiter) extends
     loadCaches(ab, i0, iN)
     tokenizer(ab, i0, math.min(iN, cachedEnd), n)
   }
-  
-  def _tok(s: String, i0: Int, iN: Int, n: Int): Long = {
+  def not(s: String, i0: Int, iN: Int): Int = {
     if (i0 >= iN) { cachedData = null; return -1-iN }
     loadCaches(s, i0, iN)
-    val ans = tokenizer._tok(s, i0, math.min(iN, cachedEnd), n)
-    if (ans.inLong.i0 == cachedEnd) { cachedData = null; return -1-cachedEnd }
-    ans
+    tokenizer.not(s, i0, iN)
   }
-  def _tok(ab: Array[Byte], i0: Int, iN: Int, n: Int): Long = {
+  def not(ab: Array[Byte], i0: Int, iN: Int): Int = {
     if (i0 >= iN) { cachedData = null; return -1-iN }
     loadCaches(ab, i0, iN)
-    val ans = tokenizer._tok(ab, i0, math.min(iN, cachedEnd), n)
-    if (ans.inLong.i0 == cachedEnd) { cachedData = null; return -1-cachedEnd }
-    ans
-  }
-  
-  def tok_(s: String, i0: Int, iN: Int, n: Int): Long = {
-    if (i0 >= iN) { cachedData = null; return -1-iN }
-    loadCaches(s, i0, iN)
-    tokenizer.tok_(s, i0, math.min(iN, cachedEnd), n)
-  }
-  def tok_(ab: Array[Byte], i0: Int, iN: Int, n: Int): Long = {
-    if (i0 >= iN) { cachedData = null; return -1-iN }
-    loadCaches(ab, i0, iN)
-    tokenizer.tok_(ab, i0, math.min(iN, cachedEnd), n)
+    tokenizer.not(ab, i0, iN)
   }
 }
 
