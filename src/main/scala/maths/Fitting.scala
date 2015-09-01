8bf1b2897a12bcb51f6fa5310675448dcd2740cd
Rex Kerr <ichoran@gmail.com>
2015-09-01 14:34:05 -0700

    Fixed obvious bugs in OLS fitting.

--- src/main/scala/maths/Fitting.scala
+++ src/main/scala/maths/Fitting.scala
@@ -5,10 +5,15 @@ package kse.maths
 
 import scala.math._
 
+trait FitWithError {
+  def error: Double
+  def samples: Double
+}
+
 /** FitTX performs a ordinary least squares fit of a parameter t against a readout x.
   * We assume t is accurate; this method is not precise if both t and x can vary.
   */
-final class FitTX {
+final class FitTX extends FitWithError {
   private[this] var Ot = 0.0
   private[this] var Ox = 0.0
 
@@ -42,6 +47,7 @@ final class FitTX {
   def samples = n
 
   def clear(): this.type = {
+    cached = false
     n = 0
     St = 0
     Stt = 0
@@ -53,8 +59,9 @@ final class FitTX {
 
   def origin(ot: Double, ox: Double): this.type = {
     if (n > 0) {
-      val dt = ot - Ot
-      val dx = ox - Ox
+      cached = false
+      val dt = Ot - ot
+      val dx = Ox - ox
       val ndt = n*dt
       val ndx = n*dx
       Stt += dt*(2*St + ndt)
@@ -91,13 +98,50 @@ final class FitTX {
     Sxx -= x*x
     Stx -= t*x
   }
+
+  override def toString = s"Fit: x = $betaX t + $alphaX (n=$n, rmse=${sqrt(error)/n}"
+}
+object FitTX {
+  def apply(ts: Array[Double], xs: Array[Double], i0: Int, iN: Int): FitTX = {
+    val fit = new FitTX
+    if (iN > i0) {
+      var i = i0
+      fit.origin(ts(i), xs(i))
+      while (i < iN) {
+        fit += (ts(i), xs(i))
+        i += 1
+      }
+      fit.origin(0, 0)
+    }
+    fit
+  }
+
+  def apply(ts: Array[Double], xs: Array[Double]): FitTX = apply(ts, xs, 0, min(ts.length, xs.length))
+
+  def apply(tf: Int => Double, xf: Int => Double, i0: Int, iN: Int): FitTX = {
+    val fit = new FitTX
+    if (iN > i0) {
+      var i = i0
+      fit.origin(tf(i), xf(i))
+      while (i < iN) {
+        fit += (tf(i), xf(i))
+        i += 1
+      }
+      fit.origin(0, 0)
+    }
+    fit
+  }
+
+  def apply(ts: Array[Float], xs: Array[Float], i0: Int, iN: Int): FitTX = apply(i => ts(i), i => xs(i), i0, iN)
+
+  def apply(ts: Array[Float], xs: Array[Float]): FitTX = apply(ts, xs, 0, min(ts.length, xs.length))
 }
 
 
 /** FitTXY performs a ordinary least squares fit of a parameter t against two readouts x and y.
   * We assume t is accurate; this method is not precise if t has error as well as x and y.
   */
-final class FitTXY {
+final class FitTXY extends FitWithError {
   private[this] var Ot = 0.0
   private[this] var Ox = 0.0
   private[this] var Oy = 0.0
@@ -140,6 +184,7 @@ final class FitTXY {
   def samples = n
 
   def clear(): this.type = {
+    cached = false
     n = 0
     St = 0
     Stt = 0
@@ -154,9 +199,10 @@ final class FitTXY {
 
   def origin(ot: Double, ox: Double, oy:Double): this.type = {
     if (n > 0) {
-      val dt = ot - Ot
-      val dx = ox - Ox
-      val dy = oy - Oy
+      cached = false
+      val dt = Ot - ot
+      val dx = Ox - ox
+      val dy = Oy - oy
       val ndt = n*dt
       val ndx = n*dx
       val ndy = n*dy
@@ -205,15 +251,52 @@ final class FitTXY {
     Stx -= t*x
     Sty -= t*y
   }
+
+  override def toString = s"Fit: x = $betaX t + $alphaX; y = $betaY t + $alphaY (n=$n, rmse=${sqrt(error)/n}"
+}
+object FitTXY {
+  def apply(ts: Array[Double], xs: Array[Double], ys: Array[Double], i0: Int, iN: Int): FitTXY = {
+    val fit = new FitTXY
+    if (iN > i0) {
+      var i = i0
+      fit.origin(ts(i), xs(i), ys(i))
+      while (i < iN) {
+        fit += (ts(i), xs(i), ys(i))
+        i += 1
+      }
+      fit.origin(0, 0, 0)
+    }
+    fit
+  }
+
+  def apply(ts: Array[Double], xs: Array[Double], ys: Array[Double]): FitTXY = apply(ts, xs, ys, 0, min(ts.length, min(xs.length, ys.length)))
+
+  def apply(tf: Int => Double, xf: Int => Double, yf: Int => Double, i0: Int, iN: Int): FitTXY = {
+    val fit = new FitTXY
+    if (iN > i0) {
+      var i = i0
+      fit.origin(tf(i), xf(i), yf(i))
+      while (i < iN) {
+        fit += (tf(i), xf(i), yf(i))
+        i += 1
+      }
+      fit.origin(0, 0, 0)
+    }
+    fit
+  }
+
+  def apply(ts: Array[Float], xs: Array[Float], ys: Array[Float], i0: Int, iN: Int): FitTXY = apply(i => ts(i), i => xs(i), i => ys(i), i0, iN)
+
+  def apply(ts: Array[Float], xs: Array[Float], ys: Array[Float]): FitTXY = apply(ts, xs, ys, 0, min(ts.length, xs.length))
 }
 
 /** FitOLS performs a ordinary least squares fit of a parameter against dim-1 other values.
   * We assume the first parameter is accurate; this method does not minimize Euclidean
   * distance to nearest line point.
   */
-final class FitOLS(dim: Int) {
+final class FitOLS(dims: Int) extends FitWithError {
   private[this] var n = 0L
-  private[this] val m = max(1, dim)
+  private[this] val m = max(1, dims)
   private[this] val O = new Array[Double](m)
   private[this] val S = new Array[Double](3*m)
   private[this] var cached = false
@@ -243,6 +326,7 @@ final class FitOLS(dim: Int) {
   def clear: this.type = {
     n = 0
     var i = 0
+    cached = false
     while (i < m) {
       O(i) = 0
       i += 1
@@ -255,14 +339,15 @@ final class FitOLS(dim: Int) {
     this
   }
 
-  def origin(ori: Array[Double]): this.type = {
+  def origin(ori: Array[Double], offset: Int = 0): this.type = {
     if (n > 0) {
+      cached = false
       val St = S(0)
-      val dt = ori(0) - O(0)
+      val dt = O(0) - ori(offset)
       val ndt = n*dt
       var j = 1
       while (j < m) {
-        val dxj = ori(j) - O(j)
+        val dxj = O(j) - ori(j + offset)
         val ndxj = n*dxj
         S(m+j) += dxj*(2*S(j) + ndxj)
         S(2*m+j) += dt*S(j) + dxj*(St + ndt)
@@ -270,24 +355,24 @@ final class FitOLS(dim: Int) {
         j += 1
       }
       S(m) += dt*(2*St + ndt)
-      S(0) + ndt
+      S(0) += ndt
     }
     var i = 0
     while (i < m) {
-      O(i) = ori(i)
+      O(i) = ori(i + offset)
       i += 1
     }
     this
   }
 
-  def +=(data: Array[Double], i0: Int = 0) {
+  def +=(data: Array[Double], offset: Int = 0) {
     cached = false
     n += 1
     var j = 0
-    var i = i0
-    val t = data(i0)
+    var i = offset
+    val t = data(offset) - O(0)
     while (j < m) {
-      val xi = data(i)
+      val xi = data(i) - O(j)
       S(2*m+j) += t*xi
       S(m+j) += xi*xi
       S(j) += xi
@@ -296,14 +381,14 @@ final class FitOLS(dim: Int) {
     }
   }
 
-  def -=(data: Array[Double], i0: Int = 0) {
+  def -=(data: Array[Double], offset: Int = 0) {
     cached = false
     n -= 1
     var j = 0
-    var i = i0
-    val t = data(i0)
+    var i = offset
+    val t = data(offset) - O(0)
     while (j < m) {
-      val xi = data(i)
+      val xi = data(i) - O(j)
       S(2*m+j) -= t*xi
       S(m+j) -= xi*xi
       S(j) -= xi
@@ -311,4 +396,49 @@ final class FitOLS(dim: Int) {
       i += 1
     }
   }
+
+  override def toString = (1 until m).map(i => s"x($i) = ${beta(i)} t + ${alpha(i)}").mkString("Fit: ", "; ", s" (n=$n, err=$error)")
+}
+object FitOLS {
+  def apply(dims: Int, xs: Array[Double], i0: Int, iN: Int): FitOLS = {
+    val m = max(1, dims)
+    val fit = new FitOLS(m)
+    if (iN > i0) {
+      var i = i0
+      fit.origin(xs, i*m)
+      while (i < iN) {
+        fit += (xs, i*m)
+        i += 1
+      }
+      fit.origin(new Array[Double](m))
+    }
+    fit
+  }
+
+  def apply(dims: Int, xs: Array[Double]): FitOLS = apply(dims, xs, 0, xs.length / max(1, dims))
+
+  def apply(dims: Int, xf: (Int, Int) => Double, i0: Int, iN: Int): FitOLS = {
+    val m = max(1, dims)
+    val fit = new FitOLS(m)
+    if (iN > i0) {
+      val arr = Array.tabulate(m)(j => xf(i0, j))
+      var i = i0
+      fit.origin(arr, 0)
+      while (i < iN) {
+        var j = 0; while (j < m) { arr(j) = xf(i, j); j += 1 }
+        fit += (arr, 0)
+        i += 1
+      }
+      var j = 0; while (j < m) { arr(j) = 0.0; j += 1 }
+      fit.origin(arr, 0)
+    }
+    fit
+  }
+
+  def apply(dims: Int, xs: Array[Float], i0: Int, iN: Int): FitOLS = {
+    val m = max(1, dims)
+    apply(dims, (i, j) => xs(i*m + j), i0, iN)
+  }
+
+  def apply(dims: Int, xs: Array[Float]): FitOLS = apply(dims, xs, 0, xs.length / max(1, dims))
 }
