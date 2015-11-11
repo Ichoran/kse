ad430bb46b227ba521cbebcc428a7d12c162246d
Rex Kerr <ichoran@gmail.com>
2015-11-10 17:16:18 -0800

    Default serialization mostly done.  Need to fix string quoting.

--- src/main/scala/jsonic/Ast.scala
+++ src/main/scala/jsonic/Ast.scala
@@ -7,22 +7,83 @@ sealed trait JsResult
 
 sealed trait JsVal extends JsResult
 
-case object JsNull extends JsVal
+case object JsNull extends JsVal { override def toString = "null" }
 
 sealed trait JsBool extends JsVal { def value: Boolean }
-case object JsTrue extends JsBool { def value = true }
-case object JsFalse extends JsBool { def value = false }
+case object JsTrue extends JsBool { def value = true; override def toString = "true" }
+case object JsFalse extends JsBool { def value = false; override def toString = "false" }
 
-final case class JsStr(value: String) extends JsVal {}
-object JsStr { def empty = new JsStr("") }
+final case class JsStr(value: String) extends JsVal { override def toString = JsStr.quoted(value) }
+object JsStr {
+  def empty = new JsStr("")
+  def quoted(s: String): String = '"' + s + '"'
+  def escaped(s: String): String = s
+}
 
-final case class JsNum(value: Double, literal: String) extends JsVal {}
-object JsNum { def nan = new JsNum(Double.NaN, "null") }
+final case class JsNum(value: Double, literal: String) extends JsVal { override def toString = literal }
+object JsNum {
+  def nan = new JsNum(Double.NaN, "null")
+  def approx(value: Double): String = ???
+}
 
 sealed trait JsArr extends JsVal { def values: Array[JsVal] }
-final case class JsArrV(values: Array[JsVal]) extends JsArr {}
+final case class JsArrV(values: Array[JsVal]) extends JsArr {
+  override def toString = {
+    val parts = new Array[String](values.length)
+    var i = 0
+    var n = 0
+    while (i < parts.length) { parts(i) = values(i).toString; n += parts(i).length; i += 1 }
+    val text = new Array[Char](2 + n + math.max(parts.length-1, 0)*2)
+    i = 0
+    n = 1
+    text(0) = '['
+    if (parts.length > 0) {
+      parts(0).getChars(0, parts(0).length, text, 1)
+      n += parts(0).length
+      i = 1
+    }
+    while (i < parts.length) {
+      text(n) = ','
+      text(n+1) = ' '
+      parts(i).getChars(0, parts(i).length, text, n+2)
+      n += parts(i).length + 2
+      i += 1
+    }
+    text(n) = ']'
+    new String(text)
+  }
+}
 final case class JsArrD(doubles: Array[Double]) extends JsArr {
   def values = { var i = 0; val v = new Array[JsVal](doubles.length); while (i < doubles.length) { v(i) = JsNum(doubles(i), doubles(i).toString); i += 1 }; v }
+  override def toString = {
+    val parts = new Array[String](doubles.length)
+    var i = 0
+    var n = 0
+    while (i < parts.length) {
+      val vi = doubles(i)
+      parts(i) = if (vi.isNaN || vi.isInfinite) "null" else vi.toString
+      n += parts(i).length
+      i += 1
+    }
+    val text = new Array[Char](2 + n + math.max(parts.length-1, 0)*2)
+    i = 0
+    n = 1
+    text(0) = '['
+    if (parts.length > 0) {
+      parts(0).getChars(0, parts(0).length, text, 1)
+      n += parts(0).length
+      i = 1
+    }
+    while (i < parts.length) {
+      text(n) = ','
+      text(n+1) = ' '
+      parts(i).getChars(0, parts(i).length, text, n+2)
+      n += parts(i).length + 2
+      i += 1
+    }
+    text(n) = ']'
+    new String(text)
+  }
 }
 object JsArr { def empty: JsArr = new JsArrV(new Array[JsVal](0)) }
 
@@ -36,6 +97,50 @@ final case class JsObj(keys: Array[String], values: Array[JsVal], table: collect
     None
   }
   def hasDuplicateKeys = table.size < keys.length
+  override def toString = {
+    val parts = new Array[String](2 * values.length)
+    var i, j = 0
+    var n = 0
+    while (i < keys.length) { 
+      val ks = JsStr.escaped(keys(i))
+      val vs = values(i).toString
+      n += ks.length + 3 + vs.length
+      parts(j) = ks
+      j += 1
+      parts(j) = vs
+      j += 1
+      i += 1
+    }
+    val text = new Array[Char](2 + n + math.max(parts.length-2, 0))
+    j = 0
+    n = 1
+    text(0) = '{'
+    if (parts.length > 1) {
+      text(1) = '"'
+      parts(0).getChars(0, parts(0).length, text, 2)
+      n += 1 + parts(0).length
+      text(n) = '"'
+      text(n+1) = ':'
+      parts(1).getChars(0, parts(1).length, text, n+2)
+      n += 2 + parts(1).length
+      j = 2
+    }
+    while (j < parts.length) {
+      text(n) = ','
+      text(n+1) = ' '
+      text(n+2) = '"'
+      parts(j).getChars(0, parts(j).length, text, n+3)
+      n += 3 + parts(j).length
+      j += 1
+      text(n) = '"'
+      text(n+1) = ':'
+      parts(j).getChars(0, parts(j).length, text, n+2)
+      n += 2 + parts(j).length
+      j += 1
+    }
+    text(n) = '}'
+    new String(text)
+  }
 }
 object JsObj { def empty = new JsObj(new Array[String](0), new Array[JsVal](0), Map.empty[String, JsVal]) }
 
