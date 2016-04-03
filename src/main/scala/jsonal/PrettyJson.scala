9d6bf8df8e46ed0641159be55eada09583e2b1dc
Rex Kerr <ichoran@gmail.com>
2016-04-02 20:54:53 -0700

    Prettyprinting plus visitors for Jsonal.

--- src/main/scala/jsonal/PrettyJson.scala
+++ src/main/scala/jsonal/PrettyJson.scala
@@ -4,64 +4,86 @@
 package kse.jsonal
 
 
-// TODO--this is buggy.  Fix it.
-class PrettyJson(val lines: collection.mutable.ArrayBuilder[String], val lastLine: java.lang.StringBuilder, indent: Int = 2, margin: Int = 70) {
-  private[jsonal] val myLine = new java.lang.StringBuilder
-  private var lastSpaces = 0
-  private var myIndent = math.max(0, indent)
-  private var myMargin = math.max(0, margin)
-  private var mySpaces = PrettyJson.lotsOfSpaces
-  private var myDepth = 0
-  def depth = myDepth
-  def deeper: this.type = { myDepth += 1; this }
-  def shallower: this.type = { myDepth = if (myDepth > 0) myDepth -1 else 0; this }
-  def tooDeep = lastSpaces >= myMargin
-  def append(s: String): this.type = { lastLine append s; this }
-  def pad(): this.type = {
-    if (indent-1 > mySpaces.length) mySpaces = Array.fill(indent)(' ')
-    lastLine.append(mySpaces, 0, math.max(0, indent-1))
-    this
-  }
-  def advance(depth: Int): this.type = {
-    myDepth = depth
-    val nsp = math.min(myMargin, depth * myIndent)
-    if (lastLine.length > 0) { lines += lastLine.toString; lastLine.setLength(math.min(lastSpaces, nsp)) }
-    val missing = lastLine.length - nsp
-    if (missing > 0) {
-      if (missing > mySpaces.length) mySpaces = Array.fill(mySpaces.length * 2)(' ')
-      lastLine.append(mySpaces, 0, missing)
-    }
-    lastSpaces = lastLine.length
-    this
-  }
-  def advance(): this.type = advance(myDepth)
-  def advanceWithCarryover(n: Int, depth: Int): this.type = {
-    val iN = math.max(0, math.min(n, lastLine.length))
-    lines += lastLine.substring(0, iN)
-    val nsp = math.min(myMargin, depth * myIndent)
-    if (nsp > iN) {
-      var i = lastSpaces
-      while (i < iN) { lastLine.setCharAt(i, ' '); i += 1 }
-      if (mySpaces.length < nsp-iN) mySpaces = Array.fill(nsp-iN)(' ')
-      lastLine.insert(iN, mySpaces, 0, nsp-iN)
-    }
-    else {
-      if (nsp < iN) lastLine.delete(nsp, iN)
-      if (lastSpaces < nsp) {
-        var i = lastSpaces
-        while (i < iN) { lastLine.setCharAt(i, ' '); i += 1 }
+/** This class implements default prettyprinting for JSON values. */
+class PrettyJson(indentWithTabs: Boolean = false, indentation: Int = 2, rightMargin: Int = 78) extends AbstractJsonVisitor {
+  //////////////////////////////////////
+  // This section validates the input //
+  //////////////////////////////////////
+  private[this] var spaces: Array[Char] = 
+    if (indentWithTabs) PrettyJson.aLotOfTabs
+    else                PrettyJson.aLotOfSpaces
+
+  private[this] var myMargin = if (rightMargin < 1) Int.MaxValue else rightMargin
+
+  private[this] var myIndent = if (indentation < 1) 1 else if (indentation > myMargin) myMargin
+
+  /////////////////////////////////////////////////////////////////
+  // This section handles accumulating the pretty representation //
+  /////////////////////////////////////////////////////////////////
+  private[this] var lastIndent = 0
+
+  /** Clears the accumulated pretty representation. */
+  def clear: this.type = { historic.clear; current.setLength(0); lastIndent = 0; this }
+
+  /** Contains the lines prior to the current line. */
+  val historic = collection.mutable.ArrayBuffer.empty[String]
+
+  /** Contains the current line being built. */
+  val current = new java.lang.StringBuilder
+
+  /** Appends a string to the current line. */
+  def append(s: String): this.type = { current append s; this }
+
+  /** Appends a StringBuilder to the current line. */
+  def slurp(sb: java.lang.StringBuilder): this.type  = { current append sb; sb.setLength(0); this }
+
+  /** Creates a new line indented the specified number of characters.
+    *
+    * Note: this method does not obey the `indentation` parameter.  It is assumed that the argument will be selected with the correct indentation in mind.
+    */
+  def nl(indent: Int): this.type = {
+    val in = math.max(0, indent)
+    historic += current.toString
+    if (lastIndent > 0) current.setLength(math.min(lastIndent, in))
+    val n = current.length - in
+    if (n > 0) {
+      var m = spaces.length
+      while (n < m) m = math.min(Int.MaxValue, m.toLong * 2).toInt
+      if (m > spaces.length) {
+        val a = new Array[Char](m)
+        java.util.Arrays.fill(a, spaces(0))
+        spaces = a
       }
+      current append (spaces, 0, n)
     }
-    myDepth = math.max(depth, 0)
-    lastSpaces = nsp
+    lastIndent = in
     this
   }
-  def result() = {
-    if (lastLine.length > 0) { lines += lastLine.toString ; lastLine.setLength(0) }
-    lines.result()
-  }
+
+  /** The `String` representation of the `JSON` value as built so far.  This method may be called repeatedly. */
+  def asString =
+    if (historic.isEmpty) current.toString
+    else if (current.length == 0) historic.mkString("\n")
+    else {
+      val sb = new java.lang.StringBuilder
+      historic.foreach{ h => sb append h; sb append "\n" }
+      sb append current
+      sb.toString
+    }
+
+  /** A byte array representation of the `JSON` value as built so far.  This method may be called repeatedly. */
+  def asBytes = asString.getBytes("UTF-8")
+
+  ///////////////////////////////////////////////////
+  // This section implements the JsonVisitor trait //
+  ///////////////////////////////////////////////////
+  override def begin: this.type = { clear; this }
+  override def visitNull: this.type = { current append "null"; this }
+  override def visit(truth: Boolean): this.type = { current append (if (truth) "true" else "false"); this }
+  override def visit(text: String): this.type = { Json.Str.addJsonString(current, text); this }
+  override def finish: this.type = { nl(0); this }
 }
 object PrettyJson {
-  private[jsonal] val lotsOfSpaces: Array[Char] = Array.fill(1022)(' ')
+  private [jsonal] val aLotOfSpaces = Array.fill(1024)(' ')
+  private [jsonal] val aLotOfTabs = Array.fill(1024)('\t')
 }
-
