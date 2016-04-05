1c070fafdac55c83ad3e9756f74d09b4d2b4f97d
Rex Kerr <ichoran@gmail.com>
2016-04-05 15:38:27 -0700

    Added numeric formatter to prettyprinter.
    It's not exceedingly fast, but at least it is quite pretty.
    One could do quite a bit better with a Long-and-multiplication-based
    approach, but I think it's fast enough as it is so I won't bother.

--- src/main/scala/jsonal/PrettyJson.scala
+++ src/main/scala/jsonal/PrettyJson.scala
@@ -280,3 +280,144 @@ object PrettyJson {
   private [jsonal] val aLotOfSpaces = Array.fill(1024)(' ')
   private [jsonal] val aLotOfTabs = Array.fill(1024)('\t')
 }
+
+class PrettyContextJson(indentWithTabs: Boolean = false, indentation: Int = 2, rightMargin: Int = 78)
+extends PrettyJson(indentWithTabs, indentation, rightMargin) {
+  protected var context: List[Any] = Nil   // Note - ONLY put `String` or `Int` in here!!  (Need union types!)
+  override def nextIndex(index: Int): this.type = {
+    if (index == 0) context = index :: context
+    else context = index :: context.tail
+    super.nextIndex(index)
+  }
+  override def outOfAllArr: this.type = {
+    context = context.tail
+    super.outOfAllArr
+  }
+  override def nextKey(index: Int, key: String): this.type = {
+    if (index == 0) context = key :: context
+    else context = key :: context.tail
+    super.nextKey(index, key)
+  }
+  override def outOfObj: this.type = {
+    context = context.tail
+    super.outOfObj
+  }
+}
+
+class PrettyNumberJson(contextFormatter: List[Any] => PrettyNumberJson.Formatter = PrettyNumberJson.defaultContext, rightMargin: Int = 129)
+extends PrettyContextJson(false, 2, rightMargin) {
+  private[this] var fmt: PrettyNumberJson.Formatter = null
+  override def visit(num: Json.Num): this.type = { append(contextFormatter(context) format num); this }
+  override def visit(jad: Json.Arr.Dbl): this.type = {
+    fmt = contextFormatter(context)
+    super.visit(jad)
+  }
+  override def visitDblIndex(index: Int, value: Double): this.type = {
+    if (index > 0) comma
+    append(fmt format value)
+    this
+  }
+}
+object PrettyNumberJson {
+  private[this] val eNumberFormatStrings = (0 to 18).map(i => s"%.${i}e")
+  private[this] val fNumberFormatStrings = (0 to 21).map(i => s"%.${i}f")
+  private[this] val powersOfTen = (0 to 308).map(i => s"1e$i".toDouble)
+  private[this] val reciprocalPowersOfTen = (0 to 323).map(i => s"1e-$i".toDouble)
+  trait Formatter {
+    def format(d: Double): String
+    def format(num: Json.Num): String
+  }
+  final case class FormatTo(absolutePrecisionDigit: Int, relativePrecisionDigits: Int) extends Formatter {
+    private[this] val tooTiny = ("5e-" + math.max(0, math.min(324, absolutePrecisionDigit+1))).toDouble
+    def format(d: Double): String = {
+      if (java.lang.Double.isNaN(d) || java.lang.Double.isInfinite(d)) return "null"
+      val ad = math.abs(d)
+      val zero = if (d < 0) 1 else 0
+      if (ad < tooTiny) return "0"
+      if (ad >= 1e-3 && ad < 1e7) {
+        val first =
+          if (ad >= 10) {
+            if (ad >= 1e4) {
+              if (ad >= 1e5) {
+                if (ad >= 1e6) 6
+                else 5
+              }
+              else 4
+            }
+            else if (ad >= 1e2) {
+              if (ad >= 1e3) 3
+              else 2
+            }
+            else 1
+          }
+          else if (ad >= 1e-1) {
+            if (ad >= 1) 0
+            else -1
+          }
+          else if (ad >= 1e-2) -2
+          else -3
+        val f = math.min(21, math.max(0, math.min(relativePrecisionDigits - first, absolutePrecisionDigit)))
+        val s = fNumberFormatStrings(f).format(d)
+        if (f > 0 && s.charAt(s.length-1) == '0') {
+          var i = s.length - 2
+          while (i > zero && s.charAt(i) == '0') i -= 1
+          if (i > zero && s.charAt(i) == '.') i -= 1
+          s.substring(0, i+1)
+        }
+        else s
+      }
+      else {
+        val first =
+          if (ad > 1) {
+            var k = 8
+            var inc = 2
+            while (k + inc < powersOfTen.length && ad >= powersOfTen(k+inc)) { k += inc; inc = inc*2 }
+            var l = math.min(k + inc, powersOfTen.length - 1)
+            while (l-k > 1) {
+              var x = (l+k)/2
+              if (ad >= powersOfTen(x)) k = x
+              else l = x
+            }
+            if (ad >= powersOfTen(l)) l
+            else k
+          }
+          else {
+            var k = 4
+            var inc = 2
+            while (k + inc < reciprocalPowersOfTen.length && ad < reciprocalPowersOfTen(k+inc)) { k += inc; inc = inc*2 }
+            var l = math.min(k + inc, reciprocalPowersOfTen.length - 1)
+            while (l-k > 1) {
+              var x = (l+k)/2
+              if (ad < reciprocalPowersOfTen(x)) k = x
+              else l = x
+            }
+            if (ad >= reciprocalPowersOfTen(k)) -k
+            else -l
+          }
+        val e = math.min(18, math.max(0, math.min(relativePrecisionDigits, first + absolutePrecisionDigit)))
+        val s = eNumberFormatStrings(e).format(d)
+        val i = s.lastIndexOf('e')
+        var j = i-1
+        if (j > zero && s.charAt(j) == '0') {
+          while (j > zero && s.charAt(j) == '0') j -= 1
+          if (j > zero && s.charAt(j) == '.') j -= 1
+          s.substring(0, j+1) + s.substring(i)
+        }
+        else s
+      }
+    }
+      
+    def format(jn: Json.Num): String = {
+      if (jn.isLong) jn.long.toString
+      else if (jn.isDouble) format(jn.double)
+      else jn.toString
+    }
+  }
+  def defaultContext = (xs: List[Any]) => defaultFormatter
+  val defaultFormatter: Formatter = new Formatter {
+    def format(d: Double): String =
+      if (java.lang.Double.isNaN(d) || java.lang.Double.isInfinite(d)) "null"
+      else d.toString
+    def format(num: Json.Num): String = num.toString
+  }
+}
