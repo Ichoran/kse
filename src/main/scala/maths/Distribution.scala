dbbe7a2ef81bc00480a9d36a2bdd31145ce8f843
Rex Kerr <ichoran@gmail.com>
2016-06-04 10:00:53 -0700

    Got Extremal working.  No tests.

--- src/main/scala/maths/Distribution.scala
+++ src/main/scala/maths/Distribution.scala
@@ -49,13 +49,13 @@ trait Estimate extends Deviable {
 }
 
 abstract class MutableEstimate(var n: Int, var mean: Double, var sse: Double) extends Estimate {
-  final protected def include(x: Double) {
+  final protected def incorporate(x: Double) {
     val mold = mean
     mean = (n*mean + x)/(n+1)
     sse += (x - mean)*(x - mold)
     n += 1
   }
-  final protected def include(that: MutableEstimate) {
+  final protected def incorporate(that: Estimate) {
     val nold = n
     n += that.n
     val mold = mean
@@ -71,35 +71,13 @@ final class EstM(n0: Int, mean0: Double, sse0: Double) extends MutableEstimate(n
   def ++(that: Estimate): EstM = clone ++= that
   def +:(x: Double): EstM = if (!x.nan) this else clone += x
   def :+(x: Double): EstM = if (!x.nan) this else clone += x
-  def +=(x: Double): this.type = { if (!x.nan) include(x); this }
-  def ++=(that: Estimate): this.type = { include(that); this }
+  def +=(x: Double): this.type = { if (!x.nan) incorporate(x); this }
+  def ++=(that: Estimate): this.type = { incorporate(that); this }
   override def toString = f"$mean +- $error (n=$n)"
 }
 object EstM {
   def apply(): EstM = new EstM(0, 0, 0)
   def apply(n: Int, mean: Double, sse: Double): EstM = new EstM(n, mean, sse)
-  def from(xs: Array[Double]): EstM = {
-    var i, n = 0
-    var s = 0.0
-    while (i < xs.length) { val xi = xs(i); if (!xi.nan) { s += xi; n += 1 }; i += 1 }
-    if (n < 2) return new EstM(n, s, 0)
-    val m = s / n;
-    s = 0
-    i = 0
-    while (i < xs.length) { val xi = xs(i); if (!xi.nan) s += (xi - m).sq; i += 1 }
-    new EstM(n, m, s)
-  }
-  def from(xs: Array[Float]): EstM = {
-    var i, n = 0
-    var s = 0.0
-    while (i < xs.length) { val xi = xs(i); if (!xi.nan) { s += xi; n += 1 }; i += 1 }
-    if (n < 2) return new EstM(n, s, 0)
-    val m = s / n;
-    s = 0
-    i = 0
-    while (i < xs.length) { val xi = xs(i); if (!xi.nan) s += (xi - m).sq; i += 1 }
-    new EstM(n, m, s)
-  }
 }
 
 final case class Est(n: Int, mean: Double, sse: Double) extends Estimate {
@@ -116,12 +94,13 @@ object Est {
     val iB = math.max(iA, math.min(data.length, iN))
     var cuml = 0.0
     var i = iA
-    while (i < iB) { cuml += data(i); i += 1 }
-    val mean = if (iB > iA) cuml/i else 0.0
+    var n = 0
+    while (i < iB) { val x = data(i0); if (!x.nan) { cuml += x; n += 1 }; i += 1 }
+    val mean = if (n > 0) cuml/n else 0.0
     cuml = 0.0
     i = iA
-    while (i < iB) { cuml += (data(i) - mean).sq; i += 1 }
-    new Est(iB - iA, mean, cuml)
+    while (i < iB) { val x = data(i0); if (!x.nan) { cuml += (data(i) - mean).sq }; i += 1 }
+    new Est(n, mean, cuml)
   }
   def from(data: Array[Float]): Est = from(data, 0, data.length)
   def from(data: Array[Double], i0: Int, iN: Int): Est = {
@@ -129,12 +108,13 @@ object Est {
     val iB = math.max(iA, math.min(data.length, iN))
     var cuml = 0.0
     var i = iA
-    while (i < iB) { cuml += data(i); i += 1 }
-    val mean = if (iB > iA) cuml/i else 0.0
+    var n = 0
+    while (i < iB) { val x = data(i0); if (!x.nan) { cuml += x; n += 1 }; i += 1 }
+    val mean = if (n > 0) cuml/n else 0.0
     cuml = 0.0
     i = iA
-    while (i < iB) { cuml += (data(i) - mean).sq; i += 1 }
-    new Est(iB - iA, mean, cuml)
+    while (i < iB) { val x = data(i0); if (!x.nan) { cuml += (data(i) - mean).sq }; i += 1 }
+    new Est(n, mean, cuml)
   }
   def from(data: Array[Double]): Est = from(data, 0, data.length)
 }
@@ -142,6 +122,7 @@ object Est {
 trait Extremal extends Estimate {
   def max: Double
   def min: Double
+  def central: Estimate
 }
 
 final class EstXM(n0: Int, mean0: Double, sse0: Double, var min: Double, var max: Double)
@@ -149,7 +130,8 @@ extends MutableEstimate(n0, mean0, sse0) with Extremal {
   def this() = this(0, 0, 0, 0 , 0)
   override def clone: EstXM = new EstXM(n, mean, sse, min, max)
   def immutable = new EstX(n, mean, sse, min, max)
-  def ++(that: Estimate): EstM = clone ++= that
+  def central = new EstM(n, mean, sse)
+  def ++(that: Estimate): EstM = central ++= that
   def ++(that: Extremal): EstXM = clone ++= that
   def +:(x: Double): EstXM = if (!x.nan) this else clone += x
   def :+(x: Double): EstXM = if (!x.nan) this else clone += x
@@ -158,79 +140,84 @@ extends MutableEstimate(n0, mean0, sse0) with Extremal {
       if (n < 1) { min = x; max = x }
       else if (x > max) max = x
       else if (x < min) min = x
-      include(x)
+      incorporate(x)
     }
     this
   }
-  def ++=(that: Estimate): this.type = { combine(that); this }
+  def ++=(that: Estimate): EstM = central ++= that
   def ++=(that: Extremal): this.type = {
     if (n < 1) { min = that.min; max = that.max }
     else if (that.n > 0) { min = math.min(min, that.min); max = math.max(max, that.max) }
-    combine(that)
+    incorporate(that)
     this
   }
   override def toString = f"$mean +- $error (min=$min, max=$max, n=$n)"
 }
 object EstXM {
-  def apply(): EstXM = new EstM(0, 0, 0, 0, 0)
-  def apply(n: Int, mean: Double, sse: Double, min: Double, max: Double): EstM = new EstM(n, mean, sse, min, max)
-  def apply(xs: Array[Double]): EstXM = {
-    var i, n = 0
-    var s = 0.0
-    while (i < xs.length) { val xi = xs(i); if (!xi.nan) { s += xi; n += 1 }; i += 1 }
-    if (n < 2) return new EstM(n, s, 0, s, s)
-    val m = s / n;
-    i = 0
-    var min, max = Double.NaN
-    while (i < xs.length && xs(i).nan) i += 1;
-    var min, max = xs(i)
-    s = (xs(i) - m).sq
-    i += 1
-    while (i < xs.length) {
-      val xi = xs(i)
-      if (!xi.nan) {
-        s += (xi - m).sq
-        if (xi > max) max = xi
-        else if (xi < min) min = xi
-      }
-      i += 1
-    }
-    new EstXM(n, m, s, min, max)
-  }
-  def apply(xs: Array[Float]): EstXM = {
-    var i, n = 0
-    var s = 0.0
-    while (i < xs.length) { val xi = xs(i); if (!xi.nan) { s += xi; n += 1 }; i += 1 }
-    if (n < 2) return new EstM(n, s, 0, s, s)
-    val m = s / n;
-    i = 0
-    var min, max = Double.NaN
-    while (i < xs.length && xs(i).nan) i += 1;
-    var min, max = xs(i)
-    s = (xs(i) - m).sq
-    i += 1
-    while (i < xs.length) {
-      val xi = xs(i)
-      if (!xi.nan) {
-        s += (xi - m).sq
-        if (xi > max) max = xi
-        else if (xi < min) min = xi
-      }
-      i += 1
-    }
-    new EstXM(n, m, s, min, max)
-  }
+  def apply(): EstXM = new EstXM(0, 0, 0, 0, 0)
+  def apply(n: Int, mean: Double, sse: Double, min: Double, max: Double): EstXM = new EstXM(n, mean, sse, min, max)
 }
 
 final case class EstX(n: Int, mean: Double, sse: Double, min: Double, max: Double) extends Extremal {
   def mutable = new EstXM(n, mean, sse, min, max)
-  def +:(x: Double): Est = (mutable += x).immutable
-  def :+(x: Double): Est = (mutable += x).immutable
+  def central = new Est(n, mean, sse)
+  def +:(x: Double): EstX = (mutable += x).immutable
+  def :+(x: Double): EstX = (mutable += x).immutable
   def ++(that: Estimate): Est = (mutable ++= that).immutable
+  def ++(that: Extremal): EstX = (mutable ++= that).immutable
   override val sd = math.sqrt(variance)
   override val error = math.sqrt(errorSq)
 }
-
+object EstX {
+  def from(data: Array[Float], i0: Int, iN: Int): EstX = {
+    val iA = math.max(0, i0)
+    val iB = math.max(iA, math.min(data.length, iN))
+    var cuml = 0.0
+    var i = iA
+    var n = 0
+    while (i < iB) { val x = data(i0); if (!x.nan) { cuml += x; n += 1 }; i += 1 }
+    val mean = if (n > 0) cuml/n else 0.0
+    if (n < 2) return EstX(n, mean, 0, mean, mean)
+    cuml = 0.0
+    i = iA
+    var min, max = mean
+    while (i < iB) {
+      val x = data(i0)
+      if (!x.nan) { 
+        cuml += (data(i) - mean).sq
+        if (x > max) max = x
+        else if (x < min) min = x
+      }
+      i += 1
+    }
+    new EstX(n, mean, cuml, min, max)
+  }
+  def from(data: Array[Float]): EstX = from(data, 0, data.length)
+  def from(data: Array[Double], i0: Int, iN: Int): EstX = {
+    val iA = math.max(0, i0)
+    val iB = math.max(iA, math.min(data.length, iN))
+    var cuml = 0.0
+    var i = iA
+    var n = 0
+    while (i < iB) { val x = data(i0); if (!x.nan) { cuml += x; n += 1 }; i += 1 }
+    val mean = if (n > 0) cuml/n else 0.0
+    if (n < 2) return EstX(n, mean, 0, mean, mean)
+    cuml = 0.0
+    i = iA
+    var min, max = mean
+    while (i < iB) {
+      val x = data(i0)
+      if (!x.nan) { 
+        cuml += (data(i) - mean).sq
+        if (x > max) max = x
+        else if (x < min) min = x
+      }
+      i += 1
+    }
+    new EstX(n, mean, cuml, min, max)
+  }
+  def from(data: Array[Double]): EstX = from(data, 0, data.length)
+}
 
 /*
 trait Quantile extends Estimate {
