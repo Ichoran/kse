669f5186053618ae97bbc0871de5600b55946a04
Rex Kerr <ichoran@gmail.com>
2017-06-30 17:13:29 -0700

    Fixed bug in fix of bug for appending JSON objects.

--- src/main/scala/jsonal/Jast.scala
+++ src/main/scala/jsonal/Jast.scala
@@ -2474,9 +2474,9 @@ object Json extends FromJson[Json] with JsonBuildTerminator[Json] {
             var k = a.length
             while (i >= k - m && k < 0x7FFFFFFE) k = ((k << 1) | k) & 0x7FFFFFFE
             a = java.util.Arrays.copyOf(a, k)
-            System.arraycopy(that, i0, a, i, m)
-            i += m
           }
+          System.arraycopy(that, i0, a, i, m)
+          i += m
         }
         this
       }
