9dafa7ab5c671944e99c39703f1a992e77a5e6c7
Rex Kerr <ichoran@gmail.com>
2018-04-16 08:09:14 -0700

    Added variants to t-test and rank test to measure difference.

--- src/main/scala/maths/Statistics.scala
+++ src/main/scala/maths/Statistics.scala
@@ -7,20 +7,31 @@ import kse.coll.packed._
 
 package stats {
   object Parametric {
-    def tTestP(ea: Est, eb: Est): Double = {
+    def tTestPackedPD(ea: Est, eb: Est): Floatx2 = {
       val xa = ea.variance/ea.n
       val xb = eb.variance/eb.n
       val ss = xa + xb
       val t = (ea.mean - eb.mean)/math.sqrt(ss)
       val df = (ss*ss/(xa*xa/(ea.n - 1) + xb*xb/(eb.n - 1))).rint.toLong
-      2*cdfStudentT(df, t)
+      Floats((2*cdfStudentT(df, t)).toFloat, (ea.mean - eb.mean).toFloat)
     }
+    def tTestP(ea: Est, eb: Est): Double = tTestPackedPD(ea, eb).f0.toDouble
+    def tTestPD(ea: Est, eb: Est): (Double, Double) = { val pt = tTestPackedPD(ea, eb); (pt.f0.toDouble, pt.f1.toDouble) }
+
+    def tTestPackedPD(a: Array[Double], b: Array[Double]): Floatx2 = tTestPackedPD(Est from a, Est from b)
     def tTestP(a: Array[Double], b: Array[Double]): Double = tTestP(Est from a, Est from b)
+    def tTestPD(a: Array[Double], b: Array[Double]): (Double, Double) = tTestPD(Est from a, Est from b)
+    
+    def tTestPackedPD(a: Array[Float], b: Array[Float]): Floatx2 = tTestPackedPD(Est from a, Est from b)
     def tTestP(a: Array[Float], b: Array[Float]): Double = tTestP(Est from a, Est from b)
+    def tTestPD(a: Array[Float], b: Array[Float]): (Double, Double) = tTestPD(Est from a, Est from b)
   }
 
   object Nonparametric {
-    private def rankTestEncoded(ab: Array[Double], na: Int): Double = {
+    /** Returns both an estimate of the p-value and the signed rank biserial correlation
+      * (with negative values indicating that the first data set is lower than the second)
+      */
+    private def rankTestEncoded(ab: Array[Double], na: Int): Floatx2 = {
       val nb = ab.length - na
       java.util.Arrays.sort(ab)
       var ra = 0.0
@@ -56,9 +67,19 @@ package stats {
       val correction = if (t3_t > 0) t3_t/((na+nb)*(na+nb-1)) else 0
       val dev = (mid*(na + nb + 1 - correction)/6).sqrt
       val z = math.abs(u - mid)/dev
-      2*cdfNormal(-z)
+      val p = 2*cdfNormal(-z)
+      val rbc = {
+        // Kind of silly to remove signs and then add them back again, but
+        // it's easier to follow textbook/Wikipedia math that way
+        val rr = ab.length*0.5*(ab.length + 1)
+        val unsign = 1 - (if (u > mid) 2*mid - u else u)/mid
+        if (ra < rr)     -unsign
+        else if (ra > rr) unsign
+        else              0
+      }
+      Floats(p.toFloat, rbc.toFloat)
     }
-    def rankTestP(a: Array[Double], b: Array[Double]): Double = {
+    def rankTestPackedPR(a: Array[Double], b: Array[Double]): Floatx2 = {
       val c = new Array[Double](a.length + b.length)
       var i = 0
       while (i < a.length) {
@@ -71,7 +92,7 @@ package stats {
       }
       rankTestEncoded(c, a.length)
     }
-    def rankTestP(a: Array[Float], b: Array[Float]): Double = {
+    def rankTestPackedPR(a: Array[Float], b: Array[Float]): Floatx2 = {
       val c = new Array[Double](a.length + b.length)
       var i = 0
       while (i < a.length) {
@@ -84,17 +105,29 @@ package stats {
       }
       rankTestEncoded(c, a.length)
     }
+    def rankTestP(a: Array[Double], b: Array[Double]): Double = rankTestPackedPR(a, b).f0.toDouble
+    def rankTestP(a: Array[Float], b: Array[Float]): Double = rankTestPackedPR(a, b).f0.toDouble
+    def rankTestR(a: Array[Double], b: Array[Double]): Double = rankTestPackedPR(a, b).f1.toDouble
+    def rankTestR(a: Array[Float], b: Array[Float]): Double = rankTestPackedPR(a, b).f1.toDouble
+    def rankTestPR(a: Array[Double], b: Array[Double]): (Double, Double) = { val pr = rankTestPackedPR(a, b); (pr.f0.toDouble, pr.f1.toDouble) }
+    def rankTestPR(a: Array[Float], b: Array[Float]): (Double, Double) = { val pr = rankTestPackedPR(a, b); (pr.f0.toDouble, pr.f1.toDouble) }
   }
 }
 package object stats {
   implicit class DoubleStatisticalTesting(private val underlying: Array[Double]) extends AnyVal {
     def tTestP(that: Array[Double]): Double = Parametric.tTestP(underlying, that)
     def rankTestP(that: Array[Double]): Double = Nonparametric.rankTestP(underlying, that)
+    def rankTestR(that: Array[Double]): Double = Nonparametric.rankTestR(underlying, that)
+    def rankTestPR(that: Array[Double]): (Double, Double) = Nonparametric.rankTestPR(underlying, that)
+    def rankTestPackedPR(that: Array[Double]): Floatx2 = Nonparametric.rankTestPackedPR(underlying, that)
     def est = Est from underlying
   }
   implicit class FloatStatisticalTesting(private val underlying: Array[Float]) extends AnyVal {
     def tTestP(that: Array[Float]): Double = Parametric.tTestP(underlying, that)
     def rankTestP(that: Array[Float]): Double = Nonparametric.rankTestP(underlying, that)
+    def rankTestR(that: Array[Float]): Double = Nonparametric.rankTestR(underlying, that)
+    def rankTestPR(that: Array[Float]): (Double, Double) = Nonparametric.rankTestPR(underlying, that)
+    def rankTestPackedPR(that: Array[Float]): Floatx2 = Nonparametric.rankTestPackedPR(underlying, that)
     def est = Est from underlying
   }
 }
